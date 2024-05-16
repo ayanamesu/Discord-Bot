5ceddda262f21453a8cb31325f6da2553f88e959
Wing Lee (Jacky) <81396481+ayanamesu@users.noreply.github.com>
2024-05-16 07:49:07 -0700

    Update unit_tests.py
    5/16/24

--- milestones/Milestone5/unit_tests.py
+++ milestones/Milestone5/unit_tests.py
@@ -4,4 +4,14 @@
 # without using the functionality provided by your bot. Once all your tests passed, then you can
 # integrate these functions with your bot logic in main.py
 
+# staff_shift = Update_staff_shifts('THURS-FRI' , 'staff_id'))
+# print(staff_shift)
 
+#print(get_descending_order()))
+
+#res = insert.terminal(1, term, main, 1, 3, 2, 3, 20USD)
+#print(res)
+
+# staff = staff.all()
+# for staffs in staff:
+#     print(staff.name + ' ' + staff.last_name)
